47059373c2a4b8ab53fbd562adbc11010a08606b
App Academy Student <email@email.com>
2019-12-09 17:59:02 -0500

    fixed user.rb syntax issue... more

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@
 class User < ApplicationRecord
   validates :account_name, :email, :password_digest, :session_token, presence: true
   validates :account_name, :email, :session_token, uniqueness: true
-  validate :password, length: {minimum: 6, allow_nil: true}
+  validates :password, length: {minimum: 6, allow_nil: true}
   after_initialize :ensure_session_token
   attr_reader :password
 
