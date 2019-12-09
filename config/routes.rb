ba34c161809a1b6c69c9cbd7750787ddba520bfc
App Academy Student <email@email.com>
2019-12-09 18:11:07 -0500

    added root controller and static pages html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#root"
+  
   namespace :api, defaults: { format: 'json' } do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create, :show, :update]
