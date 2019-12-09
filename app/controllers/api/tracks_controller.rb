bfd1f15464742b042afcb2fe395bfdde16851e9b
App Academy Student <email@email.com>
2019-12-09 17:06:31 -0500

    finished models and helper controller methods

---
+++ app/controllers/api/tracks_controller.rb
@@ -0,0 +1,17 @@
+class Api::TracksController < ApplicationController
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,13 @@
+class Api::UsersController < ApplicationController
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+end
