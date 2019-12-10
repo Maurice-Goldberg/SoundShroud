dcfda4466434360fe0b14c01187bced17ea439d6
App Academy Student <email@email.com>
2019-12-10 13:14:53 -0500

    implemented the first draft of forms

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,10 @@
-import {createStore, applyMiddlware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 const configureStore = (preLoadedState = {}) => {
-  return createStore(RootReducer, preLoadedState, applyMiddlware(thunk, logger));
+  return createStore(RootReducer, preLoadedState, applyMiddleware(thunk, logger));
 };
 
 export default configureStore;
