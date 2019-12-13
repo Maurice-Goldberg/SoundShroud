7a2baadea64a6a5e4c29e4d123c54dc135a3b026
App Academy Student <email@email.com>
2019-12-13 11:20:49 -0500

    finished styling my splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -13,20 +13,26 @@ class Splash extends React.Component {
         <div className="splash-panel">
           <div className="splash-panel-wrapper">
             <div className="splash-panel-carousel">
-              <figure class="panel-1">
+              <figure className="panel-1">
                 <img id="panel-img-1" src={window.charli_xcx}/>
+                <h2 className="carousel-text">SoundShroud: the future of Dark-mode Audio Streaming</h2>
               </figure>
-              <figure class="panel-2">
+              <figure className="panel-2">
                 <img id="panel-img-2" src={window.oneohtrix}/>
+                <h2 className="carousel-text">What's next in music is first on SoundShroud</h2>
               </figure>
-              <figure class="panel-3">
+              <figure className="panel-3">
                 <img id="panel-img-3" src={window.bjork} />
+                <h2 className="carousel-text">Discover more with SoundShroud Go+</h2>
+                <h2></h2>
               </figure>
-              <figure class="panel-1">
+              <figure className="panel-1">
                 <img id="panel-img-1" src={window.charli_xcx} />
+                <h2 className="carousel-text">SoundShroud: the future of Dark-mode Audio Streaming</h2>
               </figure>
-              <figure class="panel-2">
+              <figure className="panel-2">
                 <img id="panel-img-2" src={window.oneohtrix} />
+                <h2 className="carousel-text">What's next in music is first on SoundShroud</h2>
               </figure>
             </div>
           </div>
@@ -40,6 +46,20 @@ class Splash extends React.Component {
             <Modal />
           </div>
         </nav>
+        <input id="search-bar" type="text"
+          placeholder="Search for artists, bands, tracks, podcasts" />
+          <p id="or">or</p>
+          <button id="upload-btn">Upload for free</button>
+        <h1 id="banner-text">Hear what's trending for free in the SoundShroud community</h1>
+        <div id="trending-tracks">
+          
+        </div>
+        <div id="splash-footer">
+          <Modal />
+          <h2 id="thank-you-text">Thanks for listening. Now join in.</h2>
+          <h3 id="thank-you-subheader">Save tracks, follow artists, and build playlists. All for free.</h3>
+          <p id="small-text">Already have an account?</p>
+        </div>
       </div>
     )
   }
