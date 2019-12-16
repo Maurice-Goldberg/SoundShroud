b68c18da730131810a7830a43f9c6c76776ede6a
App Academy Student <email@email.com>
2019-12-16 10:55:21 -0500

    finally properly center splash page on all browser sizes

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -10,55 +10,56 @@ class Splash extends React.Component {
     return (
       <div className="splash-div">
         <div className="top-border"></div>
-        <div className="splash-panel">
-          <div className="splash-panel-wrapper">
-            <div className="splash-panel-carousel">
-              <figure className="panel-1">
-                <img id="panel-img-1" src={window.charli_xcx}/>
-                <h2 className="carousel-text">SoundShroud: the future of Dark-mode Audio Streaming</h2>
-              </figure>
-              <figure className="panel-2">
-                <img id="panel-img-2" src={window.oneohtrix}/>
-                <h2 className="carousel-text">What's next in music is first on SoundShroud</h2>
-              </figure>
-              <figure className="panel-3">
-                <img id="panel-img-3" src={window.bjork} />
-                <h2 className="carousel-text">Discover more with SoundShroud Go+</h2>
-                <h2></h2>
-              </figure>
-              <figure className="panel-1">
-                <img id="panel-img-1" src={window.charli_xcx} />
-                <h2 className="carousel-text">SoundShroud: the future of Dark-mode Audio Streaming</h2>
-              </figure>
-              <figure className="panel-2">
-                <img id="panel-img-2" src={window.oneohtrix} />
-                <h2 className="carousel-text">What's next in music is first on SoundShroud</h2>
-              </figure>
+        <div className="splash-content">
+          <div className="splash-panel">
+            <div className="splash-panel-wrapper">
+              <div className="splash-panel-carousel">
+                <figure className="panel-1">
+                  <img id="panel-img-1" src={window.charli_xcx}/>
+                  <h2 className="carousel-text">SoundShroud: the future of Dark-mode Audio Streaming</h2>
+                </figure>
+                <figure className="panel-2">
+                  <img id="panel-img-2" src={window.oneohtrix}/>
+                  <h2 className="carousel-text">What's next in music is first on SoundShroud</h2>
+                </figure>
+                <figure className="panel-3">
+                  <img id="panel-img-3" src={window.bjork} />
+                  <h2 className="carousel-text">Discover more with SoundShroud Go+</h2>
+                  <h2></h2>
+                </figure>
+                <figure className="panel-1">
+                  <img id="panel-img-1" src={window.charli_xcx} />
+                  <h2 className="carousel-text">SoundShroud: the future of Dark-mode Audio Streaming</h2>
+                </figure>
+                <figure className="panel-2">
+                  <img id="panel-img-2" src={window.oneohtrix} />
+                  <h2 className="carousel-text">What's next in music is first on SoundShroud</h2>
+                </figure>
+              </div>
             </div>
+            <nav className="nav-bar">
+              <figure className="logo">
+                <img id="logo-img" src={window.logo} />
+                <h1 id="site-title">SOUNDSHROUD</h1>
+              </figure>
+              <div className="splash-modal-btns">
+                <Modal />
+              </div>
+            </nav>
           </div>
-          <nav className="nav-bar">
-            <figure className="logo">
-              <img id="logo-img" src={window.logo} />
-              <h1 id="site-title">SOUNDSHROUD</h1>
-            </figure>
-            <div className="splash-modal-btns">
-              <Modal />
+            <input id="search-bar" type="text"
+              placeholder="Search for artists, bands, tracks, podcasts" />
+              <p id="or">or</p>
+              <button id="upload-btn">Upload for free</button>
+            <h1 id="banner-text">Hear what's trending for free in the SoundShroud community</h1>
+            <div id="trending-tracks">
             </div>
-          </nav>
-        </div>
-        <input id="search-bar" type="text"
-          placeholder="Search for artists, bands, tracks, podcasts" />
-          <p id="or">or</p>
-          <button id="upload-btn">Upload for free</button>
-        <h1 id="banner-text">Hear what's trending for free in the SoundShroud community</h1>
-        <div id="trending-tracks">
-          
-        </div>
-        <div id="splash-footer">
-          <Modal />
-          <h2 id="thank-you-text">Thanks for listening. Now join in.</h2>
-          <h3 id="thank-you-subheader">Save tracks, follow artists, and build playlists. All for free.</h3>
-          <p id="small-text">Already have an account?</p>
+            <div id="splash-footer">
+              <Modal />
+              <h2 id="thank-you-text">Thanks for listening. Now join in.</h2>
+              <h3 id="thank-you-subheader">Save tracks, follow artists, and build playlists. All for free.</h3>
+              <p id="small-text">Already have an account?</p>
+            </div>
         </div>
       </div>
     )
